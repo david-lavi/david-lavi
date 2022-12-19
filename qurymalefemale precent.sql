5a1dcfc2970599961d35041e1fb14fd768550b0d
david-lavi <dvddvkn2@gmail.com>
2022-12-19 14:08:09 +0200

    Add files via upload

--- qurymalefemale precent.sql
+++ qurymalefemale precent.sql
@@ -1,5 +1,6 @@
-
-select cover.ClaimCause,cover.gender,cover.paidamount,100*cover.paidamount/sum(cover.paidamount) over() as prc
+/*this quarry orders insurance claims by cause and showes the sum of the  paid amount to each claim cause as well as the percentage of 
+of the paid amount from the overall paid amount partitioned by gander*/
+select cover.ClaimCause,cover.gender,cover.paidamount,100*cover.paidamount/sum(cover.paidamount) over() as [percentage]
 from
 (
 select ClaimCause,[dbo].[Member].gender as gender,sum(claimpaidamount) as paidamount
